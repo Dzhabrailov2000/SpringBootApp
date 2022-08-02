5c886ab9fa2601b7781305d35d94d951d2b5c7ee
Dzhabrailov2000 <104578715+Dzhabrailov2000@users.noreply.github.com>
2022-08-02 21:02:04 +0300

    Initial commit

---
+++ src/main/java/bootapp/service/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import web.models.User;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserDao userDao;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    public User getUserById(long id) {
+        return userDao.getUserById(id);
+    }
+
+    @Transactional
+    @Override
+    public void removeUserById(long id) {
+        userDao.removeUserById(id);
+    }
+
+    @Transactional
+    @Override
+    public void updateUserById(long id, User user) {
+        userDao.updateUserById(id, user);
+    }
+
+    @Transactional
+    @Override
+    public void saveUser(User user) {
+        userDao.saveUser(user);
+    }
+}
